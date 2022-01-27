19de0b7d274c3d1403c791a2fd596fe5d47af664
Alexander Steindorfer <98160805+AlexanderSteindorfer@users.noreply.github.com>
2022-01-27 17:38:27 +0100

    Update test function.

--- binary_search_def.py
+++ binary_search_def.py
@@ -15,26 +15,25 @@ def binary_search(numbers_list, target, left, right):
         return binary_search(numbers_list, target, mid+1, right)
 
 
-def test_binary_search():
-    """Example for demonstration. The randomly generated list will stay the same.
-    Adapt search_target to any number you want to search for.
-    """
-
-    numbers = []
-    search_target = 8
-
-    random.seed("RandomSeed")
-
-    for _ in range(1000):
-        numbers.append(random.randint(0, 1000))
-
-    numbers.sort()
-
-    if binary_search(numbers, search_target, 0, len(numbers)-1) >= 0:
-        print("Number", search_target, "found!\nIndex:", binary_search(numbers, search_target, 0, len(numbers) - 1))
-    else:
-        print(search_target, "not found!")
-
-
 if __name__ == '__main__':
+
+    def test_binary_search():
+        """Example for demonstration. The randomly generated list will stay the same.
+        Adapt search_target to any number you want to search for.
+        """
+        numbers = []
+        search_target = 8
+
+        random.seed("RandomSeed")
+
+        for _ in range(1000):
+            numbers.append(random.randint(0, 1000))
+
+        numbers.sort()
+
+        if binary_search(numbers, search_target, 0, len(numbers) - 1) >= 0:
+            print("Number", search_target, "found!\nIndex:", binary_search(numbers, search_target, 0, len(numbers) - 1))
+        else:
+            print(search_target, "not found!")
+
     test_binary_search()
